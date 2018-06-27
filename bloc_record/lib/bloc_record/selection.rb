bdb953d89f303d9b5cd2d7ed2377ce84df82ac39
Thomas Conkwright <tmconkwright@gmail.com>
2018-06-27 07:08:27 -0400

    Select Assignment Completed

--- bloc_record/lib/bloc_record/selection.rb
+++ bloc_record/lib/bloc_record/selection.rb
@@ -3,6 +3,10 @@ require 'sqlite3'
 module Selection
     def find(*ids)
 
+        ids.each do |id|
+            number_check(id)
+        end
+
         if ids.length == 1
             find_one(ids.first)
         else
@@ -16,6 +20,9 @@ module Selection
     end
 
     def find_one(id)
+        
+        number_check(id)
+
         row = connection.get_first_row <<-SQL
             SELECT #{columns.join ","} FROM #{table}
             WHERE id = #{id};
@@ -34,6 +41,9 @@ module Selection
     end
 
     def take(num=1)
+
+        number_check(num)
+
         if num > 1
             rows = connection.execute <<-SQL
                 SELECT #{columns.join ","} FROM #{table}
@@ -83,6 +93,45 @@ module Selection
         rows_to_array(rows)
     end
     
+    def number_check(num)
+        unless num.is_a? Integer && num > 0
+            raise ArgumentError.new('You must use a positive integer for that')
+        end
+    end
+
+    def method_missing(m, *args, &block)
+        if m[0..7] == "find_by_"
+           attribute = m[8...m.length] 
+           find_by(attribute, args[0])
+        else
+            puts " '.#{m}' is not properly defined method.  Please try something else."
+        end
+    end
+
+
+    def find_each(options)
+        rows = connection.execute <<-SQL
+            SELECT #{column.join(",")} FROM #{table}
+            ORDER BY id ASC
+            LIMIT #{options[:batch_size]} 
+            OFFSET #{options[:start]}
+        SQL
+
+        for rows in rows_to_array(rows)
+            yield row
+        end
+    end
+
+    def find_in_batches
+        rows = connection.execute <<-SQL
+            SELECT #{column.join(",")} FROM #{table}
+            ORDER BY id ASC
+            LIMIT #{options[:batch_size]} 
+            OFFSET #{options[:start]}
+        SQL
+
+        yield rows_to_array(rows)
+    end
 
     private
     def init_object_from_row(row)
